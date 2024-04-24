8e3e16c2c53cf009aa9ab1379d33c3d9a7815bff
CloudedSunXV <CloudedSunXV@gmail.com>
2024-04-24 11:38:10 -0400

    Question 1 answered

--- Question1.lua
+++ Question1.lua
@@ -1,14 +1,18 @@
 --Q1 - Fix or improve the implementation of the below methods
 
-local function releaseStorage(player)
-player:setStorageValue(1000, -1)
+-- Use constant variable to specify key's purpose
+local login_key = 1000
+
+-- Changed arguments to release storage of specified key.
+function releaseStorage(player, key)
+    player:setStorageValue(key, -1)
 end
 
 function onLogout(player)
-    if player:getStorageValue(1000) == 1 then
-    addEvent(releaseStorage, 1000, player)
-        
+    if player:getStorageValue(login_key) == 1 then
+        addEvent(releaseStorage, 1000, player, login_key) --after one second, 
+        return true --logout successful
     end
-    return true
-    end
+    return false --Player could not be found
+end
 
